33a61d86115da9aeaf4784be376a26b3c9419947
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-11 11:14:21 +0530

    Create withoutreturn.c

---
+++ withoutreturn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+void sub( );
+void main()
+{
+
+    sub();
+
+    
+}
+void sub( )
+{
+    int x,y,z;
+    x=10,y=9;
+    z=x-y;
+    printf("sub of two numbers%d",z);
+}
+
+
