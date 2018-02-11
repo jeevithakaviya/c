6e2dfce67b8410f3d876345e3660fbf52e58e2c8
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-11 11:05:54 +0530

    Create with return.c

---
+++ with return.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include<conio.h>
+int sub( int x int y)
+void main()
+{
+    int a,b,c;
+    c=sub(a,b);
+    printf("sub of two numbers%d",c);
+    
+}
+int sub( int x,int y)
+{
+    int z;
+    z=x-y;
+    return z;
+}
+
+
+
